d3fcb7f7fa32e1ffe5d2320662c2160b1e45f8af
jeefyang <jeefyjl@gmail.com>
2024-01-13 20:55:07 +0800

    修复合并后报错的问题

--- vite.config.node.ts
+++ vite.config.node.ts
@@ -6,15 +6,11 @@ import fs from "fs"
 
 import "./src/type.d.ts"
 
-const jsonStr = fs.readFileSync("./public/config.jsonc", "utf-8")
-const configjson: JConfigType = global.eval(`(${jsonStr})`)
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        host: configjson.node_dev_domain,
-        port: configjson.node_dev_port
-    },
+
     plugins: [
         ...VitePluginNode({
             adapter: 'express',

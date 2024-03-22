7e0c95ea3030a87bc3076e488385f0bd8144557a
jeefyang <jeefyjl@gmail.com>
2024-03-22 19:50:48 +0800

    微调

--- upload.bat
+++ upload.bat
@@ -6,7 +6,7 @@
 ::  - `%~dp0`:是为了兼容网络路径
 ::  - `--basedir`:遍历的文件夹,默认`./`
 ::  - `--outfile`:收集信息文件输出的文件名,默认`index.txt`,名字含有`_`将会自动分类,切记名字不能带中文
-::  - `--httpurl`:上传的网络路径,默认`http://localhost:3008/uploadfile` 
+::  - `--httpurl`:上传的网络路径,默认`http://localhost:3007/uploadfile` 
 start python %~dp0upload.py --basedir=%~dp0 --outfile=file.txt
 
 @REM pause

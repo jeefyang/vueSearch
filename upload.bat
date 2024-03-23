638d67f7bf37b2aa5e04d7e6aac9b302a2ad7502
jeefyang <jeefyjl@gmail.com>
2024-03-23 16:35:23 +0800

    修改上传装置

--- upload.bat
+++ upload.bat
@@ -7,6 +7,7 @@
 ::  - `--basedir`:遍历的文件夹,默认`./`
 ::  - `--outfile`:收集信息文件输出的文件名,默认`index.txt`,名字含有`_`将会自动分类,切记名字不能带中文
 ::  - `--httpurl`:上传的网络路径,默认`http://localhost:3007/uploadfile` 
+::  - `--ignorefile`:忽略规则文件,没有则不过滤
 start python %~dp0upload.py --basedir=%~dp0 --outfile=file.txt
 
 @REM pause
--- upload.py
+++ upload.py
@@ -3,70 +3,103 @@ from codecs import encode
 import os
 import stat
 import json
-import sys,getopt
+import sys
+import getopt
 from pathlib import Path
+import re
+import fnmatch
 
+big_data: list[str] = [""]
+ignore_data: list[str] = []
+basedir = "./"
 
-big_data:tuple[str]=[""]
 
 def main(argv):
-    http_url="http://localhost:3008/uploadfile"
-    basedir="./"
-    outfile="index.txt"
+    global ignore_data
+    global basedir
 
-    opts,args=getopt.getopt(argv,"",['basedir=','outfile=',"httpurl="])
-    for opt,arg in opts:
+    http_url = "http://localhost:3008/uploadfile"
+
+    outfile = "index.txt"
+    ignorefile = ""
+
+    opts, args = getopt.getopt(
+        argv, "", ['basedir=', 'outfile=', "httpurl=", "ignorefile="])
+    for opt, arg in opts:
         if opt in ("--basedir"):
-            basedir=arg
+            basedir = arg
         elif opt in ("--outfile"):
-            outfile=arg
+            outfile = arg
         elif opt in ("--httpurl"):
-            http_url=arg
-    big_data[0]+="?"+basedir+"?"
+            http_url = arg
+        elif opt in ("--ignorefile"):
+            ignorefile = arg
 
+    big_data[0] += "?"+basedir+"?"
+
+    print(basedir)
     print(http_url)
     print(outfile)
+    print(ignorefile)
+
+    with open(os.path.join(basedir, ignorefile), "r", encoding="utf-8")as f:
+        ignore_data = f.read().splitlines()
 
     loopFile(basedir)
-    with open(os.path.join(basedir,outfile),"w",encoding="utf-8")as file:
+    with open(os.path.join(basedir, outfile), "w", encoding="utf-8")as file:
         file.write(big_data[0])
         file.close()
 
-    post_data(http_url,os.path.join(basedir,outfile))
+    post_data(http_url, os.path.join(basedir, outfile))
 
 
-def post_data(http_url:str, file_url:str):
-    files={"file":open(file_url,"rb")}
-    res=requests.post(http_url,files=files)
+def post_data(http_url: str, file_url: str):
+    files = {"file": open(file_url, "rb")}
+    res = requests.post(http_url, files=files)
     data = res.text
     print(data)
 
 
-def loopFile(dir:str,relate_dir:str=""):
-    print("正在遍历:",dir)
-    filesys=os.scandir(dir)
+def check_ignore(n: str):
+
+    for k in ignore_data:
+        if fnmatch.fnmatch(n, k):
+            return True
+    return False
+
+
+def loopFile(dir: str, relate_dir: str = ""):
+    print("正在遍历:", dir)
+    filesys = os.scandir(dir)
 
     for entry in filesys:
-        new_url=entry.path
+        new_url = entry.path
+        relate_url = os.path.join(relate_dir, entry.name)
+        if check_ignore(relate_url):
+            continue
         # s=os.stat(new_url)
-        s=Path.stat(Path(new_url))
-        relate_url=os.path.join(relate_dir,entry.name)
-        file_msg=""
+        s = Path.stat(Path(new_url))
+
+        file_msg = ""
         if entry.is_dir() is True:
-            file_msg="0"+"*"+relate_url+"*"+str(s.st_size)+"*"+str(s.st_atime)+"*"+str(s.st_ctime)+"*"+str(s.st_mtime)+"|"
+            file_msg = "0"+"*"+relate_url+"*" + \
+                str(s.st_size)+"*"+str(s.st_atime)+"*" + \
+                str(s.st_ctime)+"*"+str(s.st_mtime)+"|"
             # 测试
         elif entry.is_file() is True:
-            file_msg="1"+"*"+relate_url+"*"+str(s.st_size)+"*"+str(s.st_atime)+"*"+str(s.st_ctime)+"*"+str(s.st_mtime)+"|"
+            file_msg = "1"+"*"+relate_url+"*" + \
+                str(s.st_size)+"*"+str(s.st_atime)+"*" + \
+                str(s.st_ctime)+"*"+str(s.st_mtime)+"|"
             # 测试
-        if file_msg!="":
-            big_data[0]+=file_msg
+        if file_msg != "":
+            big_data[0] += file_msg
         if entry.is_dir() is True:
-            loopFile(new_url,relate_url)
+            loopFile(new_url, relate_url)
+            pass
     filesys.close()
     return
 
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
-   print('打完收工')
-
-
+    main(sys.argv[1:])
+    print('打完收工')
